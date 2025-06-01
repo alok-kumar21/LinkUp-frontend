8eca0f181753fd7e30ec95d26342637a740e37c9
Alok Kumar <69728237+alok-kumar21@users.noreply.github.com>
2025-06-01 20:22:56 +0530

    Update AllEvents.jsx

--- src/pages/AllEvents.jsx
+++ src/pages/AllEvents.jsx
@@ -8,8 +8,8 @@ const AllEvents = () => {
 
   const { data, loading, error } = useFetch(
     title
-      ? `https://meet-up-backend-pied.vercel.app/events/title/${title}`
-      : `https://meet-up-backend-pied.vercel.app/events`
+      ? `https://meet-up-backend-pi.vercel.app/events/title/${title}`
+      : `https://meet-up-backend-pi.vercel.app/events`
   );
 
   // handle search event
